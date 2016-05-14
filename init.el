c111f820f373759599ed6855baafb037de1f596d
markhuyong <markhuyong@gmail.com>
2016-05-15 00:29:03 +0800

    Change init file for china

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
    dotspacemacs-distribution 'spacemacs
    dotspacemacs-enable-lazy-installation nil
    dotspacemacs-ask-for-lazy-installation nil
-   dotspacemacs-configuration-layer-path '("~/.spacemacs.d/")
+   dotspacemacs-configuration-layer-path '("~/.spacemacs.private/")
    dotspacemacs-delete-orphan-packages t
 
    dotspacemacs-configuration-layers
@@ -14,7 +14,7 @@
       auto-completion-tab-key-behavior 'cycle
       auto-completion-private-snippets-directory "~/.spacemacs.d/snippets/"
       :disabled-for org erc)
-     bibtex
+     ;;bibtex
      (c-c++ :variables c-c++-enable-clang-support t)
      clojure
      command-log
@@ -47,6 +47,10 @@
      react
      ruby
      rust
+     ;;Scala
+     scala
+     (scala :variables scala-enable-eldoc-mode t)
+     
      semantic
      (shell :variables shell-default-shell 'eshell)
      shell-scripts
@@ -90,6 +94,7 @@
    '(clj-refactor
      elfeed-org
      julia-mode
+     volatile-highlights
      vi-tilde-fringe)))
 
 (defun dotspacemacs/layers/SINTEFPC6985 ()
@@ -120,7 +125,7 @@
    dotspacemacs-major-mode-leader-key ","
    dotspacemacs-major-mode-emacs-leader-key "C-M-m"
    dotspacemacs-distinguish-gui-tab t
-   dotspacemacs-emacs-command-key "SPC"
+   dotspacemacs-command-key "SPC"
    dotspacemacs-remap-Y-to-y$ t
    dotspacemacs-retain-visual-state-on-shift t
    dotspacemacs-ex-substitute-global t
@@ -154,6 +159,46 @@
    dotspacemacs-whitespace-cleanup 'changed))
 
 (defun dotspacemacs/user-init ()
+(global-set-key (kbd "C-+") 'text-scale-increase)
+  (global-set-key (kbd "C--") 'text-scale-decrease)
+
+  (setq configuration-layer--elpa-archives
+        '(("melpai-cn" . "http://elpa.zilongshanren.com/melpa/")
+          ("org-cn"   . "http://elpa.zilongshanren.com/org/")
+          ("gnu-cn"   . "http://elpa.zilongshanren.com/gnu/")))
+  
+  ;; https://github.com/syl20bnr/spacemacs/issues/2705
+  ;; (setq tramp-mode nil)
+  (setq tramp-ssh-controlmaster-options "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+
+  ;; ss proxy. But it will cause anacond-mode failed.
+  (setq socks-server '("Default server" "127.0.0.1" 1080 5))
+  (setq evil-shift-round nil)
+  (setq byte-compile-warnings '(not obsolete))
+
+  (set-default 'server-socket-dir "~/.emacs.d/server")
+  (if (functionp 'window-system)
+      (when (and (window-system)
+                 (>= emacs-major-version 24))
+        (server-start)))
+  (custom-set-variables
+   ;; custom-set-variables was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(ansi-color-faces-vector
+     [default default default italic underline success warning error])
+   '(custom-enabled-themes (quote (tango-dark))))
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   )
+  (set-face-attribute 'default nil :height 160)
+  (toggle-frame-maximized)
+  ;;(fset 'evil-visual-update-x-selection 'ignore)
+
   (setq-default
 
    ;; Miscellaneous
@@ -239,14 +284,15 @@
      "http://xkcd.com/rss.xml")
 
    ;; IRC
-   erc-server-list
-   `(("irc.gitter.im" :port "6667" :nick "TheBB" :full-name ,bb/full-name
-      :ssl t :password ,bb/gitter-pwd)
-     ("irc.freenode.net" :port "6667" :nick "TheBB" :full-name ,bb/full-name))
-   erc-autojoin-channels-alist
-   '(("1\\.0\\.0" "#syl20bnr/spacemacs" "#syl20bnr/spacemacs-devel") ; Gitter
-     ("irc.gitter.im" "#syl20bnr/spacemacs" "#syl20bnr/spacemacs-devel")
-     ("freenode\\.net" "#emacs" "#emacs-beginners" "#spacemacs" "#evil-mode"))))
+   ;;erc-server-list
+   ;;`(("irc.gitter.im" :port "6667" :nick "TheBB" :full-name ,bb/full-name
+   ;;   :ssl t :password ,bb/gitter-pwd)
+   ;;  ("irc.freenode.net" :port "6667" :nick "TheBB" :full-name ,bb/full-name))
+   ;;erc-autojoin-channels-alist
+   ;;'(("1\\.0\\.0" "#syl20bnr/spacemacs" "#syl20bnr/spacemacs-devel") ; Gitter
+   ;;  ("irc.gitter.im" "#syl20bnr/spacemacs" "#syl20bnr/spacemacs-devel")
+   ;;  ("freenode\\.net" "#emacs" "#emacs-beginners" "#spacemacs" "#evil-mode"))
+   ))
 
 (defun dotspacemacs/user-config ()
 
@@ -394,3 +440,80 @@
 
 (when (file-exists-p "~/local.el")
   (load "~/local.el"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(org-agenda-files nil)
+ '(package-selected-packages
+   (quote
+    (uuidgen typit mmt thrift rake org-download livid-mode skewer-mode live-py-mode link-hint helm-hoogle flyspell-correct eyebrowse evil-visual-mark-mode evil-ediff eshell-z company-shell column-enforce-mode clojure-snippets volatile-highlights ensime sbt-mode scala-mode2 yaml-mode xterm-color ws-butler wolfram-mode window-numbering which-key web-mode web-beautify vimrc-mode use-package typo toml-mode toc-org tagedit stickyfunc-enhance stan-mode srefactor spotify spacemacs-theme smooth-scrolling smeargle slim-mode shm shell-pop scss-mode scad-mode sass-mode rvm ruby-tools ruby-test-mode ruby-end rubocop rspec-mode robe restart-emacs rbenv ranger rainbow-delimiters racer quelpa qml-mode pyvenv pytest pyenv-mode py-yapf pony-mode pip-requirements persp-mode paradox page-break-lines pacmacs orgit org-repo-todo org-present org-pomodoro org-plus-contrib org-bullets open-junk-file omnisharp nginx-mode neotree nameless multi-term move-text monokai-theme mmm-mode matlab-mode markdown-toc magit-gitflow magit-gh-pulls macrostep lua-mode lorem-ipsum linum-relative leuven-theme less-css-mode kivy-mode json-mode js2-refactor js-doc jade-mode info+ indent-guide ido-vertical-mode ibuffer-projectile hy-mode hungry-delete htmlize hl-todo hindent highlight-parentheses highlight-numbers highlight-indentation help-fns+ helm-themes helm-swoop helm-spotify helm-pydoc helm-projectile helm-mode-manager helm-make helm-gitignore helm-flyspell helm-flycheck helm-flx helm-descbinds helm-dash helm-css-scss helm-company helm-c-yasnippet helm-ag haskell-snippets google-translate golden-ratio gnuplot github-clone github-browse-file gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gist gh-md flycheck-rust flycheck-pos-tip flycheck-haskell flx-ido fish-mode fill-column-indicator fasd fancy-battery expand-region exec-path-from-shell evil-visualstar evil-tutor evil-surround evil-snipe evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-args evil-anzu ess-smart-equals ess-R-object-popup ess-R-data-view eshell-prompt-extras esh-help erc-yt erc-view-log erc-terminal-notifier erc-social-graph erc-image erc-hl-nicks emoji-cheat-sheet-plus emmet-mode elisp-slime-nav elfeed-web elfeed-goodies ein editorconfig disaster diff-hl defproject define-word dash-at-point dactyl-mode cython-mode cuda-mode company-web company-tern company-statistics company-racer company-quickhelp company-ghc company-emoji company-cabal company-c-headers company-auctex company-anaconda command-log-mode coffee-mode cmm-mode cmake-mode clean-aindent-mode clang-format cider-eval-sexp-fu cider chruby bundler buffer-move bracketed-paste auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile arduino-mode alchemist aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell 2048-game))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip-annotation ((t (:foreground "#ff9eb8"))))
+ '(company-tooltip-annotation-selection ((t (:background "#66d9ef"))))
+ '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil))))
+ '(erc-timestamp-face ((t (:inherit font-lock-comment-face :foreground nil))))
+ '(evil-search-highlight-persist-highlight-face ((t (:background "#fc5fef" :foreground "#000000"))))
+ '(flycheck-fringe-error ((t (:background nil))))
+ '(flycheck-fringe-info ((t (:background nil))))
+ '(flycheck-fringe-warning ((t (:background nil))))
+ '(font-latex-sectioning-0-face ((t (:inherit default :height 1.0 :weight bold))))
+ '(font-latex-sectioning-1-face ((t (:inherit default :height 1.0 :weight bold))))
+ '(font-latex-sectioning-2-face ((t (:inherit default :height 1.0 :weight bold))))
+ '(font-latex-sectioning-3-face ((t (:inherit default :height 1.0 :weight bold))))
+ '(font-latex-sectioning-4-face ((t (:inherit default :height 1.0 :weight bold))))
+ '(font-latex-sectioning-5-face ((t (:inherit default :height 1.0 :weight bold))))
+ '(font-latex-slide-title-face ((t (:inherit default :height 1.0 :weight bold))))
+ '(font-lock-builtin-face ((t (:foreground "#ff9eb8" :weight semi-bold))))
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-doc-face ((t (:slant italic))))
+ '(font-lock-keyword-face ((t (:weight bold))))
+ '(font-lock-string-face ((t (:slant italic))))
+ '(font-lock-warning-face ((t (:underline nil))))
+ '(header-line ((t (:box (:color "#555555" :line-width 1)))))
+ '(helm-ff-prefix ((t (:background nil :foreground "#666666" :weight bold))))
+ '(helm-prefarg ((t (:foreground "PaleGreen"))))
+ '(info-title-1 ((t (:inherit default :height 1.0 :weight bold))))
+ '(info-title-2 ((t (:inherit default :height 1.0 :weight bold))))
+ '(info-title-3 ((t (:inherit default :height 1.0 :weight bold))))
+ '(info-title-4 ((t (:inherit default :height 1.0 :weight bold))))
+ '(markdown-header-face ((t (:inherit default :height 1.0 :weight bold))))
+ '(markdown-header-face-1 ((t (:inherit default :height 1.0 :weight bold))))
+ '(markdown-header-face-2 ((t (:inherit default :height 1.0 :weight bold))))
+ '(markdown-header-face-3 ((t (:inherit default :height 1.0 :weight bold))))
+ '(markdown-header-face-4 ((t (:inherit default :height 1.0 :weight bold))))
+ '(markdown-header-face-5 ((t (:inherit default :height 1.0 :weight bold))))
+ '(markdown-header-face-6 ((t (:inherit default :height 1.0 :weight bold))))
+ '(mode-line ((t (:box (:color "#999999" :line-width 1 :style released-button)))))
+ '(mode-line-inactive ((t (:box (:color "#666666" :line-width 1 :style released-button)))))
+ '(org-document-title ((t (:inherit default :height 1.0 :weight bold))))
+ '(org-done ((t (:foreground "MediumSpringGreen"))))
+ '(org-level-1 ((t (:inherit default :height 1.0 :weight bold))))
+ '(org-level-2 ((t (:inherit default :height 1.0 :weight bold))))
+ '(org-level-3 ((t (:inherit default :height 1.0 :weight bold))))
+ '(org-level-4 ((t (:inherit default :height 1.0 :weight bold))))
+ '(org-level-5 ((t (:inherit default :height 1.0 :weight bold))))
+ '(org-level-6 ((t (:inherit default :height 1.0 :weight bold))))
+ '(org-level-7 ((t (:inherit default :height 1.0 :weight bold))))
+ '(org-level-8 ((t (:inherit default :height 1.0 :weight bold))))
+ '(powerline-active1 ((t (:box (:color "#999999" :line-width 1 :style released-button) :background "#5a5a5a"))))
+ '(powerline-active2 ((t (:box (:color "#999999" :line-width 1 :style released-button)))))
+ '(powerline-inactive1 ((t (:box (:color "#666666" :line-width 1 :style released-button)))))
+ '(powerline-inactive2 ((t (:box (:color "#666666" :line-width 1 :style released-button)))))
+ '(region ((t (:background "#998f84"))))
+ '(spacemacs-transient-state-title-face ((t (:background nil :foreground nil :inherit font-lock-warning-face))))
+ '(term ((t (:foreground nil :background nil))))
+ '(web-mode-comment-face ((t (:inherit font-lock-comment-face :foreground nil))))
+ '(web-mode-html-attr-name-face ((t (:inherit font-lock-variable-name-face :foreground nil))))
+ '(web-mode-html-attr-value-face ((t (:inherit font-lock-string-face :foreground nil))))
+ '(web-mode-html-tag-bracket-face ((t (:inherit web-mode-html-tag-face :foreground nil))))
+ '(web-mode-html-tag-face ((t (:inherit font-lock-builtin-face :foreground nil :weight bold)))))
